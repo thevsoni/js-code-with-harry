a8c05875cf4bc7fbfc322c0961301b0e7086ef63
Vishal Soni <vs332837@gmail.com>
2023-02-10 00:13:26 +0530

    update

---
+++ Video_21.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4,5,6];
+let input = prompt("enter a numger"); //it will not run here in this vs code then how how take input here
+input = Number.parseInt(input);
+arr.push(input);
+console.log(arr);
