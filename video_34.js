ef91a6e1d2268ddead193804e083c1cddac8d029
Vishal Soni <vs332837@gmail.com>
2023-02-14 07:25:31 +0530

    update

---
+++ video_34.js
@@ -0,0 +1,7 @@
+console.log(document.body.firstChild);
+console.log(document.body.firstElementChild);
+
+let a = document.body.firstElementChild;
+console.log(a.parentElement);
+console.log(a.parentNode);
+
---
+++ video_35.js
@@ -0,0 +1,12 @@
+console.log(document.body.firstElementChild.firstElementChild)
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t.rows);
+console.log(t.tBodies);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tHead.firstElementChild);
+console.log(t.tFoot);
+console.log(t.tBodies.rows);
+console.log(typeof t);
+console.log(typeof document);
+console.log(typeof window);
